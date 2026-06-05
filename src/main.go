ff75a564d85ba246034ae9e556a541827a0422b7
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-05 14:18:07 -0700

    Argument check

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-  fmt.Println("Hello World")
-  os.Exit(0)
+	if len(os.Args) < 2 {
+		fmt.Println("Not enough arguments")
+		os.Exit(1)
+	}
+	fmt.Println("Hello World")
+	os.Exit(0)
 }
