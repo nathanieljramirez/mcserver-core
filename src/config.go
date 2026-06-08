9b05d44dc664b9d082a9bc225d3afe70cd91e11f
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 12:40:59 -0700

    Create reusable config creation function

--- src/config.go
+++ src/config.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	Jar string `jar:"version"`
+}
+
+func createConfig(config Config) error {
+	data, err := json.MarshalIndent(config, "", " ")
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile("config.json", data, 0644)
 }
