bbe7a5dcdf075b0d4947daecc667008d3be204af
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 12:36:45 -0700

    Update create.go

--- src/create.go
+++ src/create.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func create(world string) {
 	fmt.Println(world)
 	fmt.Println("Creating server...")
+	exec.Command("java", "-jar", "paper.jar")
 	os.Mkdir(world, 0755)
 }
